1b55bb36bafa9b4043588cdb0c16f95ce440dc78
Kristoffer_Miranda <johnmiranda3985@gmail.com>
2024-08-30 22:03:47 +0800

    Initial UnitTest
    Initial test cases

---
+++ TestCases/TestCases/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCases
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestCases/TestCases/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestCases
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
